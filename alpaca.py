54bdf0b8c5a784c29e59ff66200322945a7e2f87
Hamed Adam <74796492+bashbashboy-real1@users.noreply.github.com>
2025-07-13 08:39:46 +0100

    alpaca.py

--- alpaca.py
+++ alpaca.py
@@ -1,1 +1,19 @@
+import alpaca_trade_api as trade_api    
+import json
+DATA_FILE= "equities.json"
+BASE_URL="https://paper-api.alpaca.markets/"
+key="PKRKD7RSGB6KN5J416F2"
+secret_key= "4DmEIXHe1bm3N3aVOKF268lfdQyI5IaxmDiSgujI"
+api= trade_api.REST(key,secret_key,BASE_URL, api_version="v2")
+
+
+
+def get_data(symbol):
+    try:
+        barset=api.get_latest_trade(symbol)
+        return{"price": barset.price}
+    except Exception as e:
+        return- {"price":-1}
+    
+get_data("AAPL")
 
